d3c5a8fe28d5c1cf331407afb27be006ce933be5
AjShrivstava <75617683+AjShrivstava@users.noreply.github.com>
2020-12-07 21:42:18 +0530

    Removed duplicate file
    Removed duplicate file

--- IApplication.cs
+++
@@ -1,9 +0,0 @@
-using PaymentProcessing.BusinessRules.Models;
-
-namespace PaymentProcessing.Client
-{
-    public interface IApplication
-    {
-        void ProcessPayment(Payment payment);
-    }
-}

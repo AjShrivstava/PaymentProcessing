2f7a4f197dfafefe9da1caaa28067fd919380458
AjShrivstava <75617683+AjShrivstava@users.noreply.github.com>
2020-12-07 21:44:11 +0530

    Updated payment processing client
    Updated payment processing client

--- PaymentProcessing.Client/Program.cs
+++ PaymentProcessing.Client/Program.cs
@@ -9,7 +9,15 @@ namespace PaymentProcessing.Client
     {
         static void Main(string[] args)
         {
-            
+            var payment = new Payment(new Product(1, "Microservices", ProductType.Book));
+
+            var container = ContainerConfig.Configure();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var application = scope.Resolve<IApplication>();
+                application.ProcessPayment(payment);
+            }
         }
     }
 }

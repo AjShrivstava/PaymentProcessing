8a1b8e620545608eafe0bc8e67ecd7793d92227e
AjShrivstava <75617683+AjShrivstava@users.noreply.github.com>
2020-12-07 21:16:36 +0530

    Initial commit
    Initial commit

---
+++ PaymentProcessing.Client/Program.cs
@@ -0,0 +1,15 @@
+using Autofac;
+using PaymentProcessing.BusinessRules.Enums;
+using PaymentProcessing.BusinessRules.Models;
+using System;
+
+namespace PaymentProcessing.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
